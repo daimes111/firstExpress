5fdb52d2287640fa71434fb2815949963333ef2d
Devon Aimes <aimes.devon@gmail.com>
2022-09-28 22:37:00 -0400

    Tip Calculator express application created

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ app.get('/greeting', (req, res) => {
 app.get('/greeting/:name', (req, res) => {
     res.status(200).json({msg: `Hello, ${req.params.name}`})
 })
+app.get('/tip/:total/:tipPercentage', (req, res) => {
+    res.status(200).json({msg: `${(parseInt(req.params.total) * parseInt(req.params.tipPercentage)) / 100 }`})
+})
 
 app.listen(3001, function() {
     console.log('listening to port 3001')
